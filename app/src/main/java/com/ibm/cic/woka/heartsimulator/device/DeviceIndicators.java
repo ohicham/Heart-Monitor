4213111ad9fcf0a7f60a3189d942ea4637ef5817
Mohamed Wagdy <mohamedwagdykhorshid@gmail.com>
2015-06-11 22:44:47 +0200

    drucken mein wort

---
+++ app/src/main/java/com/ibm/cic/woka/heartsimulator/device/DeviceIndicators.java
@@ -0,0 +1,43 @@
+package com.ibm.cic.woka.heartsimulator.device;
+
+/**
+ * Created by Mohamed Wagdy on 09-06-2015.
+ */
+public class DeviceIndicators {
+
+    private static DeviceIndicators instance;
+
+    private int heartRate;
+
+    private boolean activated;
+
+    private DeviceIndicators() {
+        heartRate = 80;
+        activated = false;
+    }
+
+    public static DeviceIndicators getInstance() {
+        if(instance == null) {
+            instance = new DeviceIndicators();
+        }
+
+        return instance;
+    }
+
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+}
