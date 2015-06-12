83802598de27b81d8328bb364d5c6446f0985f27
Mohamed Wagdy <mohamedwagdykhorshid@gmail.com>
2015-06-12 13:42:10 +0200

    Monitor add created

---
+++ HeartMonitor/app/src/androidTest/java/heartmonitor/woka/cic/ibm/com/heartmonitor/ApplicationTest.java
@@ -0,0 +1,13 @@
+package heartmonitor.woka.cic.ibm.com.heartmonitor;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ HeartMonitor/app/src/main/java/heartmonitor/woka/cic/ibm/com/heartmonitor/MonitorActivity.java
@@ -0,0 +1,18 @@
+package heartmonitor.woka.cic.ibm.com.heartmonitor;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MonitorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.monitor);
+    }
+
+}
